e48f914191ba492126c354f85b72e240e9937733
erikaordon <erikaog@lcg.unam.mx>
2025-02-03 19:57:00 -0600

    Visualización de genes en diferentes gráficas. Finalización de documentación del código completo, falta checar la última vez. Código inspirado del github: https://github.com/lcolladotor/rnaseq_LCG-UNAM_2025/blob/a937d208512da64e5be870b21fa04db3d00f59f9/04_recount3.R y https://github.com/lcolladotor/rnaseq_LCG-UNAM_2025/blob/a937d208512da64e5be870b21fa04db3d00f59f9/05_modelos.R
    Co-authored-by: Leonardo Collado Torres <lcolladotor@gmail.com>

--- R/script.R
+++ R/script.R
@@ -1,23 +1,32 @@
-#TITLE: 	BRAF somatic mutation contributes to intrinsic epileptogenicity in pediatric brain tumors
+#TITLE:
+#"BRAF somatic mutation contributes to intrinsic epileptogenicity
+#in pediatric brain tumors"
 
+# Para acceder a los datos de secuenciación de RNA.
 library("recount3")
 
+# Guarda en una variable los proyectos de humanos disponibles.
 human_projects <- available_projects()
 
+# Selecciona los datos que concuerdan con los buscados en el proyecto SRP153743.
 proj_info <- subset(
    human_projects,
    project=="SRP153743" & project_type=="data_sources")
 
+# Crea el objeto rse para comenzar con su manipulación y análisis.
 rse_gene_SRP153743 <- create_rse(proj_info)
 
+# Cálculo de las lecturas se crean en un assay nuevo.
 assay(rse_gene_SRP153743, "counts") <- compute_read_counts(rse_gene_SRP153743)
 
+# Se obtiene la información de los metadatos.
 rse_gene_SRP153743 <- expand_sra_attributes(rse_gene_SRP153743)
 colData(rse_gene_SRP153743)[
   ,
   grepl("^sra_attribute", colnames(colData(rse_gene_SRP153743)))
 ]
 
+# Cambia los datos a numéricos y factores.
 rse_gene_SRP153743$sra_attribute.age <- as.numeric(
   rse_gene_SRP153743$sra_attribute.age
 )
@@ -28,35 +37,48 @@ rse_gene_SRP153743$sra_attribute.isolate <- factor(
   rse_gene_SRP153743$sra_attribute.isolate
 )
 
+# Muestra la información de los metadatos en un resumen.
 summary(as.data.frame(colData(rse_gene_SRP153743)
   [
     ,
     grepl("^sra_attribute.[age|sex|isolate]", colnames(colData(rse_gene_SRP153743)))
   ]))
 
+# Selecciona la condición de los datos.
 rse_gene_SRP153743$tumor <- factor(ifelse(rse_gene_SRP153743$sra_attribute.isolate == "Tumor", "Tumor", "Normal"))
 table(rse_gene_SRP153743$tumor)
 
+# Para encontrar la proporción de lecturas asignadas a genes.
 rse_gene_SRP153743$assigned_gene_prop <- rse_gene_SRP153743$recount_qc.gene_fc_count_all.assigned / rse_gene_SRP153743$recount_qc.gene_fc_count_all.total
 summary(rse_gene_SRP153743$assigned_gene_prop)
 
+# Muestra el resumen de la proporción de lecturas asignadas a genes,
+# tanto en el caso de la condición con tumor y normal.
 with(colData(rse_gene_SRP153743), tapply(assigned_gene_prop, tumor, summary))
 
+# Antes de hacer la normalización y el filtrado de datos se guarda el objeto original.
 rse_gene_SRP153743_unfiltered <- rse_gene_SRP153743
 
+# Se realiza la visualización de la proporción de lecturas asignadas a genes.
+# Ajuste de margen para visualizar la imagen.
 par(mar = rep(2, 4))
 hist(rse_gene_SRP153743$assigned_gene_prop)
 
+# Se filtran los datos para eliminar los genes con baja expresión.
 table(rse_gene_SRP153743$assigned_gene_prop < 0.5)
 
+# Guardar los genes con alta expresión.
 rse_gene_SRP153743 <- rse_gene_SRP153743[, rse_gene_SRP153743$assigned_gene_prop >= 0.5]
 
+# Se calcula la proporción de genes que se eliminaron.
 gene_means <- rowMeans(assay(rse_gene_SRP153743, "counts"))
-
+# Resumen del objeto anterior.
 summary(gene_means)
 
+# Se eliminan los genes con baja expresión.
 rse_gene_SRP153743 <- rse_gene_SRP153743[gene_means > 0.1, ]
 
+# Porcentaje de genes eliminados.
 round(nrow(rse_gene_SRP153743) / nrow(rse_gene_SRP153743_unfiltered) * 100, 2)
 
 # ------------------------------------------------------------------------------
@@ -80,7 +102,7 @@ dge <- calcNormFactors(dge)
 
 # Expresión diferencial
 
-# Exploración de datos:
+# Exploración de datos: Gráfica
 library("ggplot2")
 ggplot(as.data.frame(colData(rse_gene_SRP153743)), aes(y=assigned_gene_prop, x=tumor)) +
   geom_boxplot() +
@@ -101,8 +123,11 @@ par(mar = rep(2, 4))
 # Crear el objeto DGEListSe produce la gráfica para visualizar los datos.
 vGene <- voom(dge, mod, plot=TRUE)
 
+# Utiliza el modelo bayesiano para el análisis de expresión diferencial.
+# Además ajusta el modelo lineal.
 eb_results <- eBayes(lmFit(vGene))
 
+# Selecciona los genes con expresión diferencial significativa.
 de_results <- topTable(
   eb_results,
   coef=2,
@@ -110,6 +135,7 @@ de_results <- topTable(
   sort.by="none"
 )
 
+# Pequeña exploración y visualización de los datos.
 dim(de_results)
 head(de_results)
 
@@ -122,4 +148,50 @@ plotMA(eb_results, coef=2)
 #volcano:
 volcanoplot(eb_results, coef=2, highlight=4, names=de_results$gene_name)
 
+#-------------------------------------------------------------------------------
+
 #Visualización de genes
+
+# Extraer los 50 genes con expresión más significativa
+exprs_heatmap <- vGene$E[rank(de_results$P.Value) <= 50, ]
+
+# Data Frame
+df <- as.data.frame(colData(rse_gene_SRP153743)[, c("tumor", "sra_attribute.age", "sra_attribute.sex")])
+
+# Nombre de las columnas del data frame
+colnames(df) <- c("Condition", "AgeGroup", "Sex")
+
+# Heatmap
+#Si no se encuentra instalado el paquete, correr la siguiente línea:
+# install.packages("pheatmap")
+library("pheatmap")
+pheatmap(
+  exprs_heatmap,
+  cluster_rows = T,
+  cluster_cols = T,
+  show_rownames = F,
+  show_colnames = F,
+  annotation_col = df,
+)
+
+library("RColorBrewer")
+
+#PCA - con la condición: Tumor / Normal
+col.group <- as.factor(df$Condition)
+levels(col.group) <- brewer.pal(nlevels(col.group), "Set1")
+
+col.group <- as.character(col.group)
+
+plotMDS(vGene$E, labels=df$Condition, col=col.group)
+
+# PCA - sexo
+col.sex <- df$Sex
+levels(col.sex) <- brewer.pal(nlevels(col.sex), "Dark2")
+col.sex <- as.character(col.sex)
+plotMDS(vGene$E, labels=df$Sex, col=col.sex)
+
+# PCA - edad
+col.age <- df$AgeGroup
+levels(col.age) <- brewer.pal(nlevels(col.age), "Set2")
+col.age <- as.character(col.age)
+plotMDS(vGene$E, labels=df$AgeGroup, col=col.age)
