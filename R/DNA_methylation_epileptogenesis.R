aabe006cb980624503414210870fc3ce2bacc3f0
erikaordon <erikaog@lcg.unam.mx>
2025-02-07 00:14:35 -0600

    Finalización de la última parte del código, falta la última revisión. Código inspirado en el GitHub: https://github.com/lcolladotor/rnaseq_LCG-UNAM_2025/blob/638bc7d97ee1de7383e45c5b479188f982e82978/05_modelos.R Co-authored-by: Leonardo Collado Torres <lcolladotor@gmail.com>

--- R/DNA_methylation_epileptogenesis.R
+++ R/DNA_methylation_epileptogenesis.R
@@ -1,3 +1,6 @@
+# TITULO:
+# "Neuronal and glial DNA methylation and gene expression changes in early epileptogenesis"
+
 # Abrir librería
 library("recount3")
 
@@ -97,3 +100,92 @@ colnames(mod)
 
 # ------------------------------------------------------------------------------
 # Comprobación del modelo estadístico:
+# Para visualizar el modelo:
+# Data frame para la visualización.
+sampleData <- data.frame(
+  treatment = rse_gene_SRP223512$treatment,
+  sra_attribute.neun = rse_gene_SRP223512$sra_attribute.neun,
+)
+# Creación de las imágenes:
+vd <- ExploreModelMatrix::VisualizeDesign(
+  sampleData = sampleData,
+  designFormula = ~ 0 + treatment + sra_attribute.neun,
+  textSizeFitted = 3
+)
+cowplot::plot_grid(plotlist=vd$plot, nrow = 1)
+
+#--------------------------------------------------------------------------------
+
+# Usar limma para el análisis de expresión diferencial:
+library("limma")
+# Crear el objeto DGEList
+# Se produce la gráfica para visualizar los datos.
+vGene <- voom(dge, mod, plot=T)
+
+# Utiliza el modelo bayesiano para el análisis de expresión diferencial
+# Además, ajusta el modelo lineal
+eb_results <- eBayes(lmFit(vGene))
+
+# Selecciona los genes con expresión diferencial significativa.
+de_results <- topTable(
+  eb_results,
+  coef=2,
+  number=nrow(rse_gene_SRP223512),
+  sort.by="none"
+)
+
+# Prqueña exploración y visualización de los datos.
+dim(de_results)
+head(de_results)
+
+# Selección de genes con una expresión diferencial significativa
+table(de_results$adj.P.Val < 0.05)
+
+# Visualización de datos - plotMA
+plotMA(eb_results, coef=2)
+
+# Visualización de datos - Volcano:
+# REVISAR!!!!!
+volcanoplot(eb_results, coef=2, highlight=4, names=de_results$gene_name)
+
+#--------------------------------------------------------------------------------
+
+# Visualización de genes
+
+# Extraer los 50 genes con expresión más significativa
+exprs_heatmap <- vGene$E[rank(de_results$P.Value) <= 50, ]
+
+# Data frame
+df <- as.data.frame(colData(rse_gene_SRP223512)[, c("treatment", "sra_attribute.neun")])
+
+# Nombre de las columnas del data frame
+colnames(df) <- c("treatment", "Neun/marcador")
+
+# Heatmap
+# Si no se encuentra instalado el paquete, correr la siguiente línea:
+# install.packages("pheatmap")
+library("pheatmap")
+pheatmap(
+  exprs_heatmap,
+  cluster_rows = T,
+  cluster_cols = T,
+  show_rownames = F,
+  show_colnames = F,
+  annotation_col = df,
+)
+
+library("RColorBrewer")
+
+# PCA - con el tratamiento: Kainate/Sham
+col.group <- as.factor(df$treatment)
+levels(col.group) <- brewer.pal(nlevels(col.group), "Set1")
+
+col.group <- as.character(col.group)
+
+plotMDS(vGene$E, labels = df$treatment, col=col.group)
+
+# PCA - con el Neun: positivo/negativo
+col.neun <- df$`Neun/marcador`
+levels(col.neun) <- brewer.pal(nlevels(col.neun), "Dark2")
+col.neun <- as.character(col.neun)
+plotMDS(vGene$E, labels = df$`Neun/marcador`, col=col.neun)
